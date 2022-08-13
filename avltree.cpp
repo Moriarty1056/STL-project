8f305617af9d85b198cc88ea4131cab6ffc1caf8
Moriarty1056 <t191524p@gmail.com>
2022-08-13 15:29:40 +0530

    hello

---
+++ avltree.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct 
+
+class avl_tree{
+
+
+public:
+
+vector<int> avl;
+int size;
+
+avl_tree()
+{
+
+}
+
+void balance_value(int x)
+{
+
+}
+
+void rotate(int x)
+{
+
+}
+
+void insert(int x)
+{
+
+
+}
+
+
+ 
+
+
+};
